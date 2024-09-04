f2ec565656d8579f5f0d8fb194710fb299ef9173
Christophe Calvet <onlyatest@mytest.com>
2024-09-04 12:39:20 +0200

    done

---
+++ server/routes/colors.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json("GET /colors");
+  });
+
+  router.get("/:name", (req, res) => {
+    colorName = req.params.name
+    res.json(`GET /colors/${colorName}`);
+  });
+
+
+module.exports = router;
